eebb9c30cae97b3c77ace02ba92ba5adb4c01df0
steven9909 <sshyun@edu.uwaterloo.ca>
2025-01-19 17:03:06 -0500

    update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -34,6 +34,9 @@ DEBUG = os.getenv("DEBUG", "False") == "True"
 
 ALLOWED_HOSTS = []
 
+RENDER_EXTERNAL_HOSTNAME = os.environ.get("RENDER_EXTERNAL_HOSTNAME")
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 # Application definition
 
